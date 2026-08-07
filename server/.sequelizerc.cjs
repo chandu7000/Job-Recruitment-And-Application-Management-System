1d8fdd3e67b59ec7f9c10d37fb3b2036eab1f414
chandu7000 <nadimintichandu700@gmail.com>
2026-08-07 15:17:38 +0530

    Configure production deployment and database migration support

--- server/.sequelizerc.cjs
+++ server/.sequelizerc.cjs
@@ -1,6 +1,6 @@
-const path = require("path");
+import path from "path";
 
-module.exports = {
+export default {
   config: path.resolve("src/config/sequelize-cli.cjs"),
   "models-path": path.resolve("src/models"),
   "seeders-path": path.resolve("src/seeders"),
